7e6a5bf0a92a347e597c54bb3fcbdc952cc454c2
Virgile Hernicot <virgile.hernicot@epfl.ch>
2017-03-20 17:39:53 +0100

    implements inode scan print but stack smashing

--- work/inodes.c
+++ work/inodes.c
@@ -1,5 +1,6 @@
 #include "inode.h"
 #include "sector.h"
+#include <string.h>
 
 /**
  * @brief read all inodes from disk and print out their content to
@@ -12,11 +13,12 @@ int inode_scan_print(const struct unix_filesystem *u){
     uint16_t inc = 0;
     uint16_t size = u->s.s_isize;
     struct inode inode;
+    memset(&inode, 0, sizeof(&inode));
     while (inc < size){
         sector_read(u->f, start+inc, &inode);
         if (inode.i_mode & IALLOC){
             printf("inode %d", inc);
-            if ((inode.i_mode & IFDIR){
+            if (inode.i_mode & IFDIR){
                 printf("(" SHORT_DIR_NAME ")");
             }else{
                 printf("(" SHORT_FIL_NAME ")");
--- work/test-inodes.c
+++ work/test-inodes.c
@@ -1,5 +1,6 @@
 #include "mount.h"
 #include "inode.h"
+#include <stdio.h>
 
 int test(struct unix_filesystem *u){
     inode_scan_print(u);
